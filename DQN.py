3ab250825e9c192e6a68c5f9223c70d44d9fd1b2
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2025-02-16 12:50:19 +0530

    Adding buffer replay memory for storing states for efficient sampling

--- DQN.py
+++ DQN.py
@@ -5,6 +5,37 @@ import torch.optim as optim
 import sys
 import os 
 
+class SharedReplayBuffer:
+    def __init__(self, max_size, input_dims):
+        self.mem_size = max_size
+        self.mem_ctr = 0
+        self.state_memory = np.zeros((max_size, *input_dims), dtype=np.float32)
+        self.next_state_memory = np.zeros((max_size, *input_dims), dtype=np.float32)
+        self.action_memory = np.zeros(max_size, dtype=np.int32)
+        self.reward_memory = np.zeros(max_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(max_size, dtype=np.bool)
+
+    def store_transition(self, state, next_state, action, reward, done):
+        index = self.mem_ctr % self.mem_size
+        self.state_memory[index] = state
+        self.next_state_memory[index] = next_state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+        self.mem_ctr += 1
+
+    def sample_batch(self, batch_size):
+        max_mem = min(self.mem_ctr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size, replace=False)
+        return (
+            self.state_memory[batch],
+            self.next_state_memory[batch],
+            self.action_memory[batch],
+            self.reward_memory[batch],
+            self.terminal_memory[batch],
+        )
+
+
 class DeepQNetwork(nn.Module):
     def __init__(self, input_dims, n_actions, lr=0.001):
         super(DeepQNetwork, self).__init__()
